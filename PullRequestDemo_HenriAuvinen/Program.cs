847303c9f95a396813491d40424a25a79e87fbe7
Henri Auvinen <fheau002@edu.xamk.fi>
2024-03-08 09:23:43 +0200

    Changed program name and some text.

--- PullRequestDemo_HenriAuvinen/Program.cs
+++ PullRequestDemo_HenriAuvinen/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Title = "PullRequestDemo";
+            Console.WriteLine("Terve, Maailma xd!");
+            Console.WriteLine("8================================D");
         }
     }
 }
